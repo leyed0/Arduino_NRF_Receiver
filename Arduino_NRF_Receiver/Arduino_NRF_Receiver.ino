53a8c17bfcb57d7387e2a5575c25f4798d038994
Orley Eduardo Schimerski dos Santos <orley1994@hotmail.com>
2017-05-31 18:39:18 -0300

    none

--- Arduino_NRF_Receiver/Arduino_NRF_Receiver.ino
+++ Arduino_NRF_Receiver/Arduino_NRF_Receiver.ino
@@ -1,25 +1,53 @@
 #include "RF24.h"
 #include "HBridge.h"
 
+#define BOTID = 0;
+
+//BOTID identifica o robo atual
+//3 bits para identificar o robo
+//1 bit para indicar direção
+
+enum opts
+{
+	COUNTERCLOCKWISE = 0b0000,
+	CLOCKWISE,
+	MOTOR0 = 0b0000,
+	MOTOR1 = 0b0010,
+	MOTOR2,
+	MOTOR3,
+	ROBOT0 = 0b0000,
+	ROBOT1 = 0b1000,
+	ROBOT2,
+	ROBOT3,
+	ROBOT4,
+	ROBOT5,
+	ROBOT6,
+	ROBOT7,
+};
 
 struct comando {
-	unsigned char opt1, verify;
+	unsigned char opt1;
 	uint8_t opt2, opt3;
 	bool dir;
 };
 
 comando comm;
 RF24 NRF(7, 8);
-byte addresses[][6] = { "1Node","2Node" };
 const uint64_t pipe = 0xE8E8F0F0E1LL;
-HBridge motor[2];
+HBridge motor[2];	 
 // the setup function runs once when you press reset or power the board
 void setup() {
 	motor[0].Set(2, 4, 10);
 	motor[1].Set(5, 6, 9);
-	Serial.begin(9600);
+	Serial.begin(115200);
 	Serial.println("Receiver!");
 	NRF.begin();
+	//new code - setup
+	NRF.setPALevel(RF24_PA_MAX);
+	NRF.setDataRate(RF24_2MBPS);
+	NRF.setChannel(124);
+	NRF.setRetries(0, 10);
+	//end of new code
 	NRF.openReadingPipe(1, pipe);
 	NRF.startListening();
 	Serial.println("Setup Ok");
@@ -29,9 +57,8 @@ void setup() {
 void loop() {
 	if (NRF.available())
 	{
-		Serial.write("receiving!\n");
 		NRF.read(&comm, sizeof(comm));
-		Serial.println("Received:");
+		Serial.println("received!");
 		switch (comm.opt1)
 		{
 		case '+':
@@ -46,9 +73,15 @@ void loop() {
 		default:
 			break;
 		}
-		Serial.println(comm.opt1);
-		Serial.println(comm.opt2);
-		Serial.println(comm.opt3);
-		Serial.println(comm.dir);
+	}
+}
+
+
+void error() {
+	while (true) {
+		digitalWrite(10, true);
+		delay(100);
+		digitalWrite(10, false);
+		delay(100);
 	}
 }
